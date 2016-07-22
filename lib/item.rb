530d383a0e990bbf23ce3b5071ec8457ab977a94
Namitha <namitha.h@gmail.com>
2016-07-22 22:46:59 +0100

    Update item.rb

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,8 @@
 class Item
-	attr_reader :product_code, :name, :price
-	def initialize(product_code, name, price)
-			@product_code = product_code;
-			@name = name;
-			@price = price;
-	end
+  attr_reader :product_code, :name, :price
+  def initialize(product_code, name, price)
+    @product_code = product_code;
+    @name = name;
+    @price = price;
+  end
 end

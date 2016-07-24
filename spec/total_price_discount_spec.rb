ebc2238346616eb8bedd0800e95e29dab238179b
Namitha <namitha.h@gmail.com>
2016-07-24 21:40:04 +0100

    Update total_price_discount_spec.rb

--- spec/total_price_discount_spec.rb
+++ spec/total_price_discount_spec.rb
@@ -1,10 +1,10 @@
 require 'total_price_discount' 
 
 describe TotalPriceDiscount do 
-	subject(:total_price) {described_class.new(60, 10)}
-	let(:items) { [double, double] }
+  subject(:total_price) {described_class.new(60, 10)}
+  let(:items) { [double, double] }
 
-	describe '#discount' do
+  describe '#discount' do
     it 'calculates the discount if above the threshold' do
       expect(total_price.discount(items, 61)).to eq 6.1
     end
